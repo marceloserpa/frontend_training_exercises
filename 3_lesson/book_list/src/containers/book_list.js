0a8e8286014e3e15d894b651058dfc78d58bf57a
marceloserpa <marcelo.serpa@ilegra.com>
2017-03-03 22:28:58 -0300

    Created Book list using React + Redux

---
+++ 3_lesson/book_list/src/containers/book_list.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
+
+class BookList extends Component {
+
+    render(){
+      return (
+        <ul className="list-group col-sm-4">
+          {this.renderList()}
+        </ul>
+      );
+    }
+
+    renderList(){
+      return this.props.books.map((book) => {
+        return (
+          <li
+            key={book.title}
+            onClick={() => this.props.selectBook(book)}
+            className="list-group-item">
+              {book.title}
+          </li>
+        );
+      });
+    }
+
+}
+
+function mapStateToProps(state){
+  return {
+    books: state.books
+  };
+}
+
+function dispatchToProps(dispatch){
+  return bindActionCreators({ selectBook }, dispatch);
+}
+
+export default connect(mapStateToProps, dispatchToProps)(BookList);
---
+++ 3_lesson/book_list/src/reducers/reducer_books.js
@@ -0,0 +1,7 @@
+export default function(){
+  return [
+    {title: 'Random  book', pages: 101},
+    {title: 'The Shining', pages: 320},
+    {title: 'Test', pages: 97}
+  ]
+};
