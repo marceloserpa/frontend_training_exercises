b186869f1cef089df844ee9347afa1f72df1146b
marceloserpa <marcelo.serpa@ilegra.com>
2017-03-19 17:26:23 -0300

    testing events

--- 4_lesson/tuiter_app/test/components/message_box.test.js
+++ 4_lesson/tuiter_app/test/components/message_box.test.js
@@ -1,6 +1,7 @@
 
 import React, { Component } from 'react';
 import { shallow, mount, render } from 'enzyme';
+import sinon from 'sinon';
 
 import MessageBox from '../../src/components/message_box';
 import { expect, renderContainer } from '../test_base';
@@ -21,5 +22,29 @@ describe('MessageBox', () => {
         const wrapper = shallow(<MessageBox name="test" />);
         expect(wrapper.find('textarea').props()).to.have.property('name', 'test');
     });    
+
+    describe('testing event inside component', () => {
+
+        it('should execute callback passed in handleChange when trigger onChange event', () => {
+            let callback = sinon.spy();
+            const component = shallow(<MessageBox handleChange={callback}/>);
+            const textarea = component.find('textarea');
+
+            textarea.simulate('change', { target: { value: 'new comment' }});
+
+            expect(callback.called).to.be.true
+        });   
+
+        it('should execute callback passed in handleClick when trigger onClick event', () => {
+            let callback = sinon.spy();
+            const component = shallow(<MessageBox handleClick={callback}/>);
+            const input = component.find('input');
+
+            input.simulate('click');
+
+            expect(callback.called).to.be.true
+        });   
+
+    });
 
 })
