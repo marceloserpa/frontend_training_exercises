df9245fa1bf882044119f2be75f895de86a7fc7e
marceloserpa <jdev.marcelo@gmail.com>
2019-08-02 16:42:11 -0300

    fix message

--- learn-english/src/app/shared/sentences.mock.ts
+++ learn-english/src/app/shared/sentences.mock.ts
@@ -4,5 +4,5 @@ export const SENTENCES_MOCK: Sentence[] = [
     { english: 'I like to learn Angular' , portuguese: 'Eu gosto de aprender Angular' },
     { english: 'How are you?' , portuguese: 'Como vai você?' },
     { english: 'We have to learn typescript' , portuguese: 'Nós precisamos aprender typescript' },
-    { english: 'Is angular better than React' , portuguese: 'Angular é melhor que React' }
+    { english: 'Angular is  better than React' , portuguese: 'Angular é melhor que React' }
 ]
